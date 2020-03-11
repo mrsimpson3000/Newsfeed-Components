18a5b44fe10e152479c41b98989ab2f75d78504d
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-03-11 15:27:53 -0400

    Created elements

--- Article/Article.js
+++ Article/Article.js
@@ -101,9 +101,21 @@ const data = [
 
   Hint: You will need to use createElement more than once here!
 
-  Your function should take either an object as it's one argument, or 5 separate arguments mapping to each piece of the data object above.
+  Your function should take either an object as it's one argument, or 5 separate arguments mapping to each piece of the data object above. */
 
-  Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
+  function articleCreator(data) {
+    const article = document.createElement('div');
+    const articleTitle = document.createElement('h2');
+    const articleDate = document.createElement('p');
+    const articleParagragh1 = document.createElement('p');
+    const articleParagragh2 = document.createElement('p');
+    const articleParagragh3 = document.createElement('p');
+    const arctileExpandButton = document.createElement('span');
+
+    return article;
+  }
+
+  /* Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
 
   Step 3: return the entire component.
 
