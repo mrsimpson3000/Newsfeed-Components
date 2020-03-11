df4473789ba7a378ab814792bc79dfb02a517626
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-03-11 18:49:29 -0400

    I believe I have reached MVP

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,13 +1,16 @@
 /* This is the data we will be using, study it but don't change anything, yet. */
 
 let menuItems = [
-  'Students',
-  'Faculty',
+  "Students",
+  "Faculty",
   "What's New",
-  'Tech Trends',
-  'Music',
-  'Log Out'
+  "Tech Trends",
+  "Music",
+  "Log Out"
 ];
+
+const header = document.querySelector(".header");
+const menuButton = document.querySelector(".menu-button");
 
 /* 
 
@@ -19,9 +22,42 @@ let menuItems = [
     </ul>
   </div>
 
-  The function takes an array as its only argument.
+  The function takes an array as its only argument. */
 
-  Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array. 
+function menuCreator(menuItems) {
+  // Creating the elements
+  const menu = document.createElement("div");
+  const menuUl = document.createElement("ul");
+
+  // Creating the HTML structure using .append
+  // menu.appendChild(menuUl);
+
+  // Adding the proper classes
+  menu.classList.add("menu");
+  menuUl.classList.add("list");
+
+  //  let myMenu = menuItems.map(element => (item) {
+
+  //  })
+  menuItems.forEach(element => {
+    const menuLi = document.createElement("li");
+    menuUl.appendChild(menuLi);
+    menuLi.textContent = element;
+  });
+
+  menu.appendChild(menuUl);
+
+  menuButton.addEventListener("click", event => {
+    menu.classList.toggle("menu--open");
+  });
+
+  return menu;
+}
+
+const menuDisplay = menuCreator(menuItems);
+header.appendChild(menuDisplay);
+
+/* Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array. 
   Add those items to the <ul>
 
   Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
